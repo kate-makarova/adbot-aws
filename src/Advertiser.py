7b6ebdf88f595972890cd2b0722cef678d8684f9
Kate Makarova <kate.makarova@hotmail.com>
2024-10-08 08:27:48 -0400

    --

--- src/Advertiser.py
+++ src/Advertiser.py
@@ -102,7 +102,7 @@ class Advertiser:
                  message='Loading known data')
 
         data = []
-        with open('./src/forums.csv') as f:
+        with open('./src/settings/forums.csv') as f:
             reader = csv.reader(f, delimiter=',', quotechar='"')
             for row in reader:
                 data.append({
