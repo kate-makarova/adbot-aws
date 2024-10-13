f87a92dcf80f3fe603575f4451378ac198d7cf8c
kate-makarova <54686234+kate-makarova@users.noreply.github.com>
2024-10-13 18:49:29 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,5 +1,8 @@
 from Advertiser import Advertiser
+import os
 
-advertiser = Advertiser()
+aws_id = os.environ.get("AWS_ID")
+aws_key = os.environ.get("AWS_KEY")
+advertiser = Advertiser(aws_id=aws_id, aws_key=aws_key)
 advertiser.custom_login(url="https://kingscross.f-rpg.me", username="Assistant", password="12345")
-advertiser.work(url="https://kingscross.f-rpg.me/viewtopic.php?id=7288", id=157, home_forum_id=1)
+advertiser.work(url="https://kingscross.f-rpg.me/viewtopic.php?id=7288", id=1, home_forum_id=157)
